e6dc48e8f1d425c50a8f0224fba33f036d2037e0
Hayley Budzinski <hayleybudzinski@gmail.com>
2020-04-30 17:29:20 -0600

    email input

--- portfolio/src/components/Contact/index.js
+++ portfolio/src/components/Contact/index.js
@@ -5,8 +5,16 @@ export function Contact() {
   return (
     <div className="jumbotron jumbotron-fluid">
     <div className="container">
-        <h1 className="display-4 text-center">Want to collab on some code? Contact me below!</h1>
-        <p className="lead text-center">Insert input fields here for when i figure out how to link to my email</p>
+        <h1 className="display-4 text-center">Want to collab on a project? Contact me below!</h1>
+        {/* <form action="https://mailthis.to/hayleybudzinski@gmail.com" 
+        method="POST"> */}
+        <input type="text" className="form-control" placeholder="Full Name" name="name"></input>
+        <input type="text" className="form-control" placeholder="Email" name="email"></input>
+        <div class="input-group">
+        <textarea class="form-control" aria-label="With textarea" placeholder="Message"></textarea>
+        </div>        
+        <button type="submit" onClick="">Send</button>
+        {/* </form> */}
     </div>
     </div>
   )
