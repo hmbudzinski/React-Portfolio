23e385139f3c62e57551efc0240154168cba3fb6
Hayley Budzinski <hayleybudzinski@gmail.com>
2020-05-18 15:05:01 -0600

    trying to deploy

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -6,9 +7,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("portfolio/build"));
 }else {
-  app.use(express.static("client/public"))
+  app.use(express.static("portfolio/public"))
 }
 
 app.use(require('./routes'))
