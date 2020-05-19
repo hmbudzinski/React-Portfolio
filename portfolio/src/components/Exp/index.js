3d868d3b8d1b9666aad45f2e7ad0a6360fe1dc57
Hayley Budzinski <hayleybudzinski@gmail.com>
2020-05-18 21:29:32 -0600

    working on styling

--- portfolio/src/components/Exp/index.js
+++ portfolio/src/components/Exp/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import TopBtn from "../TopBtn"
+import "../../styles/progress.css"
+import "../../styles/home.css"
 
-export function Exp() {
+function Exp() {
 
   return (
     <>
@@ -9,8 +11,71 @@ export function Exp() {
     <div className="container">
         <h1 className="display-4 text-center">Services</h1>
         <hr id="breakline"></hr>
-        <p className="lead text-center">Details on my expertise here</p>
+        <p className="lead text-center">Thumbnails on what i use here</p>
     </div>
+
+    <section id="about-us">
+      <div className="container">
+
+        <div className="row">
+        <div className="col-lg-12 text-center">
+        <h2 className="section-heading">About Us</h2>
+        <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+        </div>
+        </div>
+
+      <div className="row">
+        <div className="col-sm-6">
+          <div className="about-info">
+          <h2>About us</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.Ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+          <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          </div>
+        </div>
+
+        <div className="col-sm-6">
+          <h4 className="progress-title">HTML</h4>
+          <div className="progress">
+          <div className="progress-bar bar3">
+          <div className="progress-value">90%</div>
+        </div>
+      </div>
+
+      <h4 className="progress-title">JavaScript</h4>
+        <div className="progress">
+        <div className="progress-bar bar1">
+        <div className="progress-value">45%</div>
+        </div>
+        </div>
+      <h4 className="progress-title">CSS</h4>
+        <div className="progress">
+        <div className="progress-bar bar2">
+        <div className="progress-value">78%</div>
+        </div>
+        </div>
+      <h4 className="progress-title">React</h4>
+        <div className="progress">
+        <div className="progress-bar bar2">
+        <div className="progress-value">78%</div>
+        </div>
+        </div>
+      <h4 className="progress-title">Node</h4>
+        <div className="progress">
+        <div className="progress-bar bar2">
+        <div className="progress-value">78%</div>
+        </div>
+        </div>
+      <h4 className="progress-title">Wordpress</h4>
+        <div className="progress">
+        <div className="progress-bar bar1">
+        <div className="progress-value">45%</div>
+        </div>
+        </div>
+      </div>
+      </div>
+      </div>
+  </section>
+
     <TopBtn />
     </div>
     </>
--- portfolio/src/components/Work/index.js
+++ portfolio/src/components/Work/index.js
@@ -6,7 +6,6 @@ import WeatherImage from "../../styles/three.PNG"
 import BudgetImage from "../../styles/four.PNG"
 import TopBtn from "../TopBtn"
 
-
 function Work() {
 
   return (
@@ -15,29 +14,29 @@ function Work() {
         <hr id="breakline"></hr>
         
         <div className="row" id="workcontainer">
-        <div className="card">
+        <div className="card shadow-drop-2-center">
             <img src={QuarImage} className="card-img-top workimages" alt="quarantivities"></img>
             <div className="card-body">
                 <h5 className="card-title">Quarantivities</h5>
                 <a href="https://rocky-inlet-27187.herokuapp.com/" class="card-link" target="blank">Visit My App</a>
             </div>
         </div>
-        <div className="card">
+        <div className="card shadow-drop-2-center">
             <img src={WeatherImage} className="card-img-top workimages" alt="Weather"></img>
             <div className="card-body">
-                <h5 className="card-title">Weather</h5>
+                <h5 className="card-title">Weather Dashboard</h5>
                 <a href="https://hmbudzinski.github.io/WeatherApp/" className="card-link" target="blank">Visit My App</a>
             </div>
         </div>
 
-        <div className="card">
+        <div className="card shadow-drop-2-center">
             <img src={BudgetImage} className="card-img-top workimages" alt="Budget"></img>
             <div className="card-body">
                 <h5 className="card-title">Budget-Tracker</h5>
                 <a href="https://vast-hamlet-59362.herokuapp.com/" className="card-link" target="blank">Visit My App</a>
             </div>
         </div>
-        <div className="card">
+        <div className="card shadow-drop-2-center">
             <img src={NerdImage} className="card-img-top workimages" alt="Nerdify"></img>
             <div className="card-body">
                 <h5 className="card-title">Nerdify</h5>
